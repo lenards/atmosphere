5667e38ade4ffcd24dd2cc0f1889931f68d5ae7b
Evan Briones <erbriones@gmail.com>
2015-08-21 16:43:12 -0700

    add obsolete decorator

--- core/decorators.py
+++ core/decorators.py
@@ -3,6 +3,7 @@
 """
 from functools import wraps
 
+
 def broken(message):
     def broken_decorator(fn):
         @wraps(fn)
@@ -11,3 +12,13 @@ def broken(message):
             return fn(*args, **kwargs)
         return wrapper
     return broken_decorator
+
+
+def obsolete(message):
+    def obsolete_decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            logger.warn(message)
+            return fn(*args, **kwargs)
+        return wrapper
+    return obsolete_decorator
