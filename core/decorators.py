16c3af920679000626aae142e2ff365a4a1f2bd0
Evan Briones <erbriones@gmail.com>
2015-08-28 10:07:09 -0700

    update decorators to take a logger

--- core/decorators.py
+++ core/decorators.py
@@ -4,17 +4,20 @@
 from functools import wraps
 
 
-def broken(message):
+def broken(message, logger):
     def broken_decorator(fn):
         @wraps(fn)
         def wrapper(*args, **kwargs):
-            logger.error(message)
-            return fn(*args, **kwargs)
+            try:
+                return fn(*args, **kwargs)
+            except:
+                logger.error(message)
+                raise
         return wrapper
     return broken_decorator
 
 
-def obsolete(message):
+def obsolete(message, logger):
     def obsolete_decorator(fn):
         @wraps(fn)
         def wrapper(*args, **kwargs):
